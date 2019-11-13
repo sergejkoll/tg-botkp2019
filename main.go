dce5ddf19016c35caba7bc7290996e694c76488d
sergejkoll <kolesnikov.school4@gmail.com>
2019-11-14 00:16:17 +0300

    heroku ))))

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	b.Start()
 
-	b.Handle("/hello", func(m *tb.Message) {
+	b.Handle("/", func(m *tb.Message) {
 		_, err := b.Send(m.Sender, "Hi!")
 		if err != nil {
 			log.Fatal(err)
