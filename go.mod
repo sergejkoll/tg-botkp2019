09f4ba1810ec9b1718b20e9bafcff52007e0560c
sergejkoll <kolesnikov.school4@gmail.com>
2019-11-14 14:45:58 +0300

    day 2(

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sergejkoll/tg-botkp2019
 
 go 1.13
 
-require gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4 // indirect
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4 // indirect
+)
--- main.go
+++ main.go
@@ -1,40 +1,77 @@
+//package main
+//
+//import (
+//	"log"
+//	"os"
+//
+//	tb "gopkg.in/tucnak/telebot.v2"
+//)
+//
+//func main() {
+//	var (
+//		port      = os.Getenv("PORT")
+//		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+//		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+//	)
+//
+//	webhook := &tb.Webhook{
+//		Listen:   ":" + port,
+//		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+//	}
+//
+//	pref := tb.Settings{
+//		Token:  token,
+//		Poller: webhook,
+//	}
+//
+//	b, err := tb.NewBot(pref)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	b.Start()
+//
+//	b.Handle("/hello", func(m *tb.Message) {
+//		_, err := b.Send(m.Sender, "Hi!")
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	})
+//}
+
 package main
 
 import (
 	"log"
-	"os"
 
-	tb "gopkg.in/tucnak/telebot.v2"
+	"github.com/Syfaro/telegram-bot-api"
 )
 
 func main() {
-	var (
-		port      = os.Getenv("PORT")
-		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-		token     = os.Getenv("TOKEN")      // you must add it to your config vars
-	)
-
-	webhook := &tb.Webhook{
-		Listen:   ":" + port,
-		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-	}
-
-	pref := tb.Settings{
-		Token:  token,
-		Poller: webhook,
-	}
-
-	b, err := tb.NewBot(pref)
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
-	b.Start()
+	bot.Debug = true
 
-	b.Handle("/", func(m *tb.Message) {
-		_, err := b.Send(m.Sender, "Hi!")
-		if err != nil {
-			log.Fatal(err)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
 		}
-	})
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
