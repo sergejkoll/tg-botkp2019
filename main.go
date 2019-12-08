8254f863c2efd376989258e2e4c3da6cf2f32f11
… <>
2019-12-08 19:36:48 +0300

    анархия

--- main.go
+++ main.go
@@ -1,77 +1,114 @@
-//package main
-//
-//import (
-//	"log"
-//	"os"
-//
-//	tb "gopkg.in/tucnak/telebot.v2"
-//)
-//
-//func main() {
-//	var (
-//		port      = os.Getenv("PORT")
-//		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-//		token     = os.Getenv("TOKEN")      // you must add it to your config vars
-//	)
-//
-//	webhook := &tb.Webhook{
-//		Listen:   ":" + port,
-//		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-//	}
-//
-//	pref := tb.Settings{
-//		Token:  token,
-//		Poller: webhook,
-//	}
-//
-//	b, err := tb.NewBot(pref)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	b.Start()
-//
-//	b.Handle("/hello", func(m *tb.Message) {
-//		_, err := b.Send(m.Sender, "Hi!")
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//	})
-//}
-
 package main
 
 import (
+	"bytes"
 	"log"
+	"net/http"
+	"net/url"
 
-	"github.com/Syfaro/telegram-bot-api"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+func start(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	requestMessage := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет. Я телеграм-бот. Войдите или зарегистрируйтесь")
+	loginButton := tgbotapi.NewKeyboardButton("/signin")
+	registerButton := tgbotapi.NewKeyboardButton("/signup")
+	keyboard := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{loginButton, registerButton})
+	requestMessage.ReplyMarkup = keyboard
+	_, err := bot.Send(requestMessage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func login(bot *tgbotapi.BotAPI, update tgbotapi.Update, updates tgbotapi.UpdatesChannel) {
+	log.Println(update.Message.Chat.ID)
+
+	requestMessage := tgbotapi.NewMessage(update.Message.Chat.ID, "Введите логин")
+	_, err := bot.Send(requestMessage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var login string
+	for upd := range updates {
+		if upd.Message != nil {
+			login = upd.Message.Text
+			log.Println("This is user login: " + login)
+			break
+		}
+	}
+
+	log.Println(update.Message.Chat.ID)
+
+	requestMessage.Text = "Введите пароль"
+	_, err = bot.Send(requestMessage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var pass string
+	for upd := range updates {
+		if upd.Message != nil {
+			pass = upd.Message.Text
+			log.Println("This is user pass" + pass)
+			break
+		}
+	}
+
+
+	json := `{"login":"` + login + `", "password":"` + pass + `"}`
+	log.Println(json)
+	body := bytes.NewBuffer([]byte(json))
+
+	resp, err := http.Post("http://127.0.0.1:8080/login", "application/json", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(resp)
+}
+
+
+func GoBot() {
+	proxyUrl, err := url.Parse("http://51.158.123.35:8811")
+	if err != nil {
+		log.Println(err)
+	}
+	http.DefaultTransport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
+	// используя токен создаем новый инстанс бота
+	bot, err := tgbotapi.NewBotAPI("1041039490:AAGXBA0Kno3_lpYlIruQ_HzgD18kW9vCYzI")
 	if err != nil {
 		log.Panic(err)
 	}
-
-	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	// u - структура с конфигом для получения апдейтов
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
+	// используя конфиг u создаем канал в который будут прилетать новые сообщения
 	updates, err := bot.GetUpdatesChan(u)
 
+	// в канал updates прилетают структуры типа Update
+	// вычитываем их и обрабатываем
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
+		if update.Message == nil {
 			continue
 		}
-
+		// логируем от кого какое сообщение пришло
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+		// свитч на обработку комманд
+		// комманда - сообщение, начинающееся с "/"
+		switch update.Message.Command() {
+		case "start":
+			start(bot, update)
+		case "signin":
+			login(bot, update, updates)
+		case "signup":
+			log.Println(update.Message.Chat.ID)
+		}
 	}
+}
+
+func main() {
+	GoBot()
 }
